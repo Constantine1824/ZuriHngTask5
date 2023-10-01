627be75636a3cdd8197d1bd01fe50f41e5e6fe7f
Ayomide <ayomidet905@gmail.com>
2023-10-01 20:24:24 +0100

    Test cronjb script

--- build.sh
+++ build.sh
@@ -7,5 +7,18 @@ pip install --upgrade setuptools wheel
 
 poetry install
 
-python tasks.py 
+# Create a temporary crontab file
+CRONTAB_FILE=$(mktemp)
+
+# Add the cron job to the temporary crontab file
+echo "*/2 * * * * /usr/bin/python3 tasks.py" >> "$CRONTAB_FILE"
+
+# Install the temporary crontab file
+crontab "$CRONTAB_FILE"
+
+# Clean up the temporary crontab file
+rm "$CRONTAB_FILE"
+
+echo "Cron job to run $PYTHON_SCRIPT every two minutes has been set up."
+
 python manage.py collectstatic --no-input
