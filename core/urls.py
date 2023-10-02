aef58a35679bed1fea10cee085af35376084a302
Ayomide <ayomidet905@gmail.com>
2023-10-02 12:55:59 +0100

    Added end endpoint

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import create,update
+from .views import create,update, close
 
 urlpatterns = [
     # path('upload', receive_file, name='upload'),
     path('create', create, name='Create-file'),
-    path('update', update, name='update-file')
+    path('update', update, name='update-file'),
+    path('end',close,name='end')
 ]
