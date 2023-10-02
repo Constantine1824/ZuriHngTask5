384452b248e82cb678fd4a083d5bca782429bf33
Ayomide <ayomidet905@gmail.com>
2023-10-02 12:44:04 +0100

    Added storages

--- Task5/settings.py
+++ Task5/settings.py
@@ -132,6 +132,12 @@ MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STORAGES = {
+    # ...
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
